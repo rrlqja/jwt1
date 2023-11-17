f79a2bb46dbf3fc61fc647d656b886634d979613
song windows <dkclasltmf@naver.com>
2023-11-17 20:59:41 +0900

    feat: add home

---
+++ src/main/java/song/jwt1/controller/HomeController.java
@@ -0,0 +1,24 @@
+package song.jwt1.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import song.jwt1.dto.LoginDto;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "/home";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(@ModelAttribute LoginDto loginDto) {
+        return "/login";
+    }
+}
