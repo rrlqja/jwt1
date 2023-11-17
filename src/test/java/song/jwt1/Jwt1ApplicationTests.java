d8f5dc3e47b6760fc2517d5267b16e9ff33d12c4
song windows <dkclasltmf@naver.com>
2023-11-17 19:58:21 +0900

    initial commit

---
+++ src/test/java/song/jwt1/Jwt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package song.jwt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
