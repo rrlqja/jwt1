069700c102d254eac644e78d9ff7ac0360a77c33
song windows <dkclasltmf@naver.com>
2023-11-18 16:06:15 +0900

    feat: jwt utils 추가

---
+++ src/main/java/song/jwt1/util/JwtUtils.java
@@ -0,0 +1,28 @@
+package song.jwt1.util;
+
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class JwtUtils {
+
+    public static final SecretKey KEY = Jwts.SIG.HS256.key().build();
+
+    public String createJwt(Long id) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expiryTime = now.plus(Duration.ofMillis(3600000));
+
+        Timestamp timestamp = Timestamp.valueOf(expiryTime);
+
+        return Jwts.builder()
+                .issuer("TestIss")
+                .subject(String.valueOf(id))
+                .expiration(timestamp)
+                .issuedAt(Timestamp.valueOf(now))
+                .signWith(KEY)
+                .compact();
+    }
+}
